0c9b5385329f62f7daf5b4127e91cdb3d2b67eb3
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-09-24 13:20:58 +0200

    changes

--- Unidad2-Profundizacion/Actividad01-2.js
+++ Unidad2-Profundizacion/Actividad01-2.js
@@ -1,11 +1,11 @@
 'use strict'
 
-function closest(x,...args){
-    let relative = [x[0],x[1]]
+function closest(charPos,...enemy){
+    let relative = [charPos[0],charPos[1]]
     let closest = [Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY]
     let distance = Number.POSITIVE_INFINITY
-    for (let value of args){
-        let dist = Math.sqrt(Math.pow(value[0]-x[0],2)+Math.pow(value[1]-x[0],2))
+    for (let value of enemy){
+        let dist = Math.sqrt(Math.pow(value[0]-charPos[0],2)+Math.pow(value[1]-charPos[0],2))
         if (distance > dist){
             closest = [value[0], value[1]]
             distance = dist
@@ -19,5 +19,5 @@ closest(
     [59,23],
     [85,65],
     [2,2],
-    [3,3]
+    [3,6]
 )
