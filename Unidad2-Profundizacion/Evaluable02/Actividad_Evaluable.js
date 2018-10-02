40dfd5e01af4a85a7deb7ff7c528246e1ce56700
LA-BESTIA--PABL\pjord <pjorda96@gmail.com>
2018-10-02 19:29:40 +0200

    spagetti code

--- Unidad2-Profundizacion/Evaluable02/Actividad_Evaluable.js
+++ Unidad2-Profundizacion/Evaluable02/Actividad_Evaluable.js
@@ -1,6 +1,27 @@
 'use strict';
 //require('db.json');
-require('Publications.js');
+
+//require('Publications.js');
+class Publications {
+
+    constructor(title, ...author) {
+
+        //var publication = new Map();
+        var authors = new Map();
+        let cont = 0;
+
+        this.title = title;
+
+        for (auth in author) {
+            authors.set(cont, auth);
+            cont++;
+        }
+
+        this.author = authors;
+    }
+
+}
+
 const rl = require('readline-sync');
 
 
