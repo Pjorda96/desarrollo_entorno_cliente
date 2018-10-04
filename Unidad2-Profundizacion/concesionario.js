e7d9b413557691e00e5e24fc82cf8534911ec583
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-10-04 09:36:15 +0200

    ejercicio concesionario

--- Unidad2-Profundizacion/concesionario.js
+++ Unidad2-Profundizacion/concesionario.js
@@ -1,15 +1,14 @@
-const rl = require('readline-sync');
+/**
+ * Ejercicio: Concesionario
+ * Dar de alta coche
+ * Dar de baja coche
+ * Buscar un coche y mostrarlo por pantalla
+ */
 
-class Coche{
+const readline = require('readline-sync');
 
-    /**
-     * 
-     * @param {String} matricula 
-     * @param {String} marca 
-     * @param {String} modelo 
-     * @param {String} color 
-     * @param {Number} km 
-     */
+class Coche {
+
     constructor(matricula, marca, modelo, color, km) {
         this.matricula = matricula;
         this.marca = marca;
@@ -19,7 +18,7 @@ class Coche{
         this.reservado = false;
     }
 
-    getMatricula(){
+    getMatricula() {
         return this.matricula;
     }
 
@@ -27,55 +26,56 @@ class Coche{
 
 let listaCoches = [];
 let salir = false;
-
 while (!salir) {
-    console.log('Bienvenidos al sistema de gestión del concesionario');
+    console.log('Bienvenidos al sistema de gestiÃ³n del concesionario');
     console.log('1) Dar de alta coche');
-    console.log('2) Dar de baja coche');
-    console.log('3) Buscar un coche y mostrar información');
+    console.log('2) Dar de baja un coche');
+    console.log('3) Buscar un coche y mostrar informaciÃ³n');
     console.log('-1) Salir del sistema');
-    let opcion = rl.questionInt('Por favor, seleccione una de estas opciones: ');
-
+    let opcion = readline.questionInt('Por favor, seleccione una de estas opciones: ');
     if (opcion === 1) {
-        //Dar de alta un coche
-        let matricula = rl.question('Matrícula: ');
-        let marca = rl.question('Marca: ');
-        let modelo = rl.question('Modelo: ');
-        let color = rl.question('Color: ');
-        let km = rl.questionFloat('Km: ');
-        let cocheNuevo = new Coche(matricula, marca, modelo, color, km);
-
-        listaCoches.push(cocheNuevo);
+        //Dar de alta el coche
+        let matricula = readline.question('Por favor, introduce una matricula: ');
+        let marca = readline.question('Introduce una marca: ');
+        let modelo = readline.question('Introduce un modelo: ');
+        let color = readline.question('Introduce un color: ');
+        let km = readline.questionFloat('Introduce el num. de km: ');
+        let newCar = new Coche(matricula, marca, modelo, color, km);
+        listaCoches.push(newCar);
         console.log(listaCoches);
     } else if (opcion === 2) {
         //Dar de baja un coche
-        let matricula = rl.question('Por favor, introduce una matrícula: ');
+        let matricula = readline.question('Por favor, introduce una matricula:');
         let encontrado = false;
-        for (let coche of listaCoches) {
+        for (let i = 0; i < listaCoches.length; i++) {
+            let coche = listaCoches[i];
             if (coche.matricula === matricula) {
-                listaCoches.splice(matricula);
+                listaCoches.splice(i);
                 encontrado = true;
                 break;
             }
         }
-        if (encontrado === true) {
-            console.log('Coche encontrado y borrado');
+        if (encontrado) {
+            console.log('Coche encontrado y borrado del sistema'); +
+            console.log(listaCoches);
         } else {
-            console.log('Coche no encontrado');
+            console.log('Coche no encontrado en el sistema');
         }
     } else if (opcion === 3) {
         //Buscar un coche
-        let matricula = rl.question('Por favor, introduce una matrícula: ');
-        for (let coche of listaCoches){
-            if (coche.matricula === matricula){
+        let matricula = readline.question('Por favor, introduce una matricula: ');
+        for (let coche of listaCoches) {
+            if (coche.matricula === matricula) {
                 console.log(coche);
                 break;
             }
         }
-
-        //let arrayCoche = listaCoches.filter( c => c.matricula === matricula );
-        //console.log(arrayCoche[0]);
+        /**
+         * let arrayCoche = listaCoches.filter( c => c.matricula===matricula );
+         * console.log(arrayCoche[0]);
+         */
     } else if (opcion === -1) {
-        this.salir = true;
+        salir = true;
     }
+
 }
