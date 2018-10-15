5945e4b36407f334b01c2c6bd6c793e8f61ce3e6
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-10-15 13:41:27 +0200

    mods

--- Unidad2-Profundizacion/Evaluable02/Actividad_Evaluable.js
+++ Unidad2-Profundizacion/Evaluable02/Actividad_Evaluable.js
@@ -1,9 +1,39 @@
 'use strict';
+/**
+ * Para una mejor lectura y comprension del codigo:
+ * 
+ * Todas la variables y metodos referentes a clases seran en inglés
+ * Las variables de datos pedidas al usuario y resto de variables
+ * utilizadas en el codigo seran en castellano
+ */
+
 const readline = require('readline-sync');
 //var db = require('./db').db;
 const ReviewArticle = require('./ReviewArticles').ReviewArticles;
 const ConferenceArticle = require('./ConferenceArticles').ConferenceArticles;
 const ScientificPatent = require('./ScientificPatents').ScientificPatents;
+
+function insertarAutores() {
+    let autores = [];
+    let autor = readline.question('Introduce el autor: ');
+    autores.push(autor);
+    do {
+        let autor = readline.question('Introduce otro autor o pulsa enter: ');
+        if (autor !== '') {
+            autores.push(autor);
+        }
+    } while (autor == '');
+    return autores;
+}
+
+function encontrado(encontrado) {
+    if (encontrado) {
+        console.log('Publicacion encontrada y borrada del sistema');
+        //console.log(publications);
+    } else {
+        console.log('Publicacion no encontrada en el sistema');
+    }
+}
 
 let publications = [];
 let salir = false;
@@ -15,10 +45,12 @@ while (!salir) {
     console.log('4) Modificar articulos cientificos'); //revista y conferencia
     console.log('5) Modificar patentes cientificas');
     console.log('6) Buscar publicaciones cientificas');
+    console.log('7) Numero de publicaciones cientificas por autor y año');
+    console.log('8) Factor de impacto por autor y año');
     console.log('-1) Salir del sistema');
     let opcion = readline.questionInt('Por favor, seleccione una de estas opciones: ');
     if (opcion === 1) {
-        //Dar de alta una publicacion //TODO: author
+        //Dar de alta una publicacion
         console.log('Selecciona un tipo.');
         console.log('1) Aticulo de revista');
         console.log('2) Articulo de conferencia');
@@ -27,7 +59,7 @@ while (!salir) {
         let tipo = readline.questionInt('Por favor, seleccione una de estas opciones: ');
         if (tipo === 1) {
             let titulo = readline.question('Introduce el titulo: ');
-            let autor = readline.question('Introduce el autor: ');
+            let autor = insertarAutores();
             let numPags = readline.questionInt('Introduce el numero de paginas: ');
             let anyoPublicacion = readline.questionInt('Introduce el año de publicacion: ');
             let numMenciones = readline.questionInt('Introduce el numero de menciones: ');
@@ -40,7 +72,7 @@ while (!salir) {
                 publications.push(reviewArticle);
         } else if (tipo === 2 ) {
             let titulo = readline.question('Introduce el titulo: ');
-            let autor = readline.question('Introduce el autor: ');
+            let autor = insertarAutores();
             let numPags = readline.questionInt('Introduce el numero de paginas: ');
             let anyoPublicacion = readline.questionInt('Introduce el año de publicacion: ');
             let numMenciones = readline.questionInt('Introduce el numero de menciones: ');
@@ -53,7 +85,7 @@ while (!salir) {
                 publications.push(conferenceArticle);
         } else if (tipo === 3) {
             let titulo = readline.question('Introduce el titulo: ');
-            let autor = readline.question('Introduce el autor: ');
+            let autor = insertarAutores();
             let anyoPublicacion = readline.questionInt('Introduce el año de publicacion: ');
             let anyoVencimiento = readline.questionInt('Introduce el año de vencimiento: ');
 
@@ -76,20 +108,16 @@ while (!salir) {
                 break;
             }
         }
-        if (encontrado) {
-            console.log('Publicacion encontrada y borrada del sistema');
-            //console.log(publications);
-        } else {
-            console.log('Publicacion no encontrada en el sistema');
-        }
+        
+        encontrado(encontrado)
     } else if (opcion === 3) {
-        //Modificar autores  //TODO: author
+        //Modificar autores
         let titulo = readline.question('Por favor, introduce un titulo: ');
         let encontrado = false;
         for (let i = 0; i < publications.length; i++) {
             let publication = publications[i];
             if (publication.title === titulo) {
-                let autor = readline.question('Por favor, introduce un autor: ');
+                let autor = insertarAutores();
                 publications[i].author = autor;
                 encontrado = true;
 
@@ -97,12 +125,7 @@ while (!salir) {
                 break;
             }
         }
-        if (encontrado) {
-            console.log('Publicacion encontrada y autor modificado');
-            console.log(publications);
-        } else {
-            console.log('Publicacion no encontrada en el sistema');
-        }
+        encontrado(encontrado)
     } else if (opcion === 4) {
         //Modificar articulos cientificos
         let type = readline.question('Por favor, introduce el tipo:\n' +
@@ -178,12 +201,7 @@ while (!salir) {
             console.log(publications[publications.length - 1]);
         }
 
-        if (encontrado) {
-            console.log('Publicacion encontrada y modificada');
-            console.log(publications);
-        } else {
-            console.log('Publicacion no encontrada en el sistema');
-        }
+        encontrado(encontrado)
     } else if (opcion === 5) {
         //Modificar patentes cientificas
         let encontrado = false;
@@ -207,31 +225,101 @@ while (!salir) {
         }
         console.log(publications[publications.length - 1]);
 
-        if (encontrado) {
-            console.log('Publicacion encontrada y modificada');
-            console.log(publications);
-        } else {
-            console.log('Publicacion no encontrada en el sistema');
-        }
+        encontrado(encontrado)
     } else if (opcion === 6) {
-        //Búsqueda por diferentes criterios //TODO: author
+        //Búsqueda por diferentes criterios
         let autor = readline.question('Introduce el autor o pulsa enter: ');
         let anyoPublicacion = readline.questionInt('Introduce el año de publicacion o 0: ');
         let tipo = readline.question('Introduce el tipo de publicacion (Revista|Conferencia|Patente) o pulsa enter: ');
         let busqueda = [];
         let encont = false;
 
-        for (let publicacion of publications) {
-            if (autor !== '' && publicacion.getAuthor == autor) encont = true;
-            if (anyoPublicacion !== 0 && publication.getAnyoPublicacion) encont = true;
+        for (let publication of publications) {
+            if (autor !== '') {
+                for (author of publication.getAuthor()) {
+                    if ( author === autor ) {
+                        encont = true;
+                    }
+                }
+            }
+            if (anyoPublicacion !== 0 && publication.getAnyoPublicacion()) encont = true;
             if (tipo !== 0 && publication.isReview
                 || publication.isConference 
                 || publication.isPatent) encont = true;
             if (encont === true) busqueda.push(publication);
         }
         console.log(busqueda);
+    } else if (opcion === 7) {
+        //Numero de publicaciones por autor y año
+        let autor = readline.question('Introduce el autor: ');
+        let anyo = readline.questionInt('Introduce el año de inicio de busqueda: ');
+        let busqueda = 0;
+        let encont = false;
+
+        for (let publication of publications) {
+            if (autor !== '' && anyo <= publication.getAnyoPublicacion()) {
+                for (author of publication.getAuthor()) {
+                    if (author === autor) {
+                        encont = true;
+                    }
+                }
+            }
+            if (encont === true) busqueda++;
+        }
+        console.log('El numero de publicaciones de ' + autor + ' es: ' + busqueda);
+    } else if (opcion === 8) {
+        //Factor de impacto por autor y año
+        let autor = readline.question('Introduce el autor: ');
+        let anyo = readline.questionInt('Introduce el año de inicio de busqueda: ');
+        let busqueda = 0;
+        let encont = false;
+
+        for (let publication of publications) {
+            if (autor !== '' && anyo <= publication.getAnyoPublicacion()) {
+                for (author of publication.getAuthor()) {
+                    if (author === autor) {
+                        encont = true;
+                    }
+                }
+            }
+            if (encont === true) busqueda = busqueda + publication.getImpactFactor();
+        }
+        console.log('El factor de impacto de ' + autor + ' es ' + busqueda);
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    else if (opcion === 9) {
+        //Calcule el Indice-h de un autor //TODO: hacer
+        let autor = readline.question('Introduce el autor: ');
+        let anyo = readline.questionInt('Introduce el año de inicio de busqueda: ');
+        let busqueda = 0;
+        let encont = false;
+
+        for (let publication of publications) {
+            if (autor !== '' && anyo <= publication.getAnyoPublicacion()) {
+                for (author of publication.getAuthor()) {
+                    if (author === autor) {
+                        encont = true;
+                    }
+                }
+            }
+            if (encont === true) busqueda = busqueda + publication.getImpactFactor();
+        }
+        console.log('El factor de impacto de ' + autor + ' es ' + busqueda);
+    }
 
 
 
