cc75311197725f1b31f11512806593dcb147cacb
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-10-25 11:04:48 +0200

    terting con errores

--- test/example_testing/__tests__/test_fraction.js
+++ test/example_testing/__tests__/test_fraction.js
@@ -20,33 +20,163 @@ test('Testing simplify II', () => {
     expect(fr.getDenominator()).toBe(5);
 });
 
-test('Testing mcd', () => {
-    //el mcd es 1
+/*
+# Testing de mcd
+*/
+
+test('Testing mcd: es 1', () => {
     expect(Fraction.mcd(4, 3)).toBe(1);
-    //el mcd es 2 (caso inicial del bucle)
-    expect(Fraction.mcd(22, 30)).toBe(2);
-    //el mcd es uno de los numeros
-    expect(Fraction.mcd(3, 9)).toBe(3);
-    expect(Fraction.mcd(9, 3)).toBe(3);
-    // entre 2 y uno de los dos parámetros 
-    //de la función(no incluídos)
-    expect(Fraction.mcd(6, 9)).toBe(3);
-    //el primer parámetro no es un entero
-    expect(() => Fraction.mcd(1.5, 5)).toThrowError(Error);
-    //el segunod parámetro no es un entero
-    expect(() => Fraction.mcd(5, 1.5)).toThrowError(Error);
 });
 
-test('Testing mcm', () => {
-    // el mcm es uno de los parámetros
+test('Testing mcd: es 2 (caso inicial del bucle)', () => {
+    expect(Fraction.mcd(22, 30)).toBe(2);
+});
+
+test('Testing mcd: es uno de los numeros', () => {
+    expect(Fraction.mcd(3, 9)).toBe(3);
+    expect(Fraction.mcd(9, 3)).toBe(3);
+});
+
+test('Testing mcd: entre 2 y uno de los dos parámetros de la función(no incluídos)', () => {
+    expect(Fraction.mcd(6, 9)).toBe(3);
+});
+
+test('Testing mcd: el primer parámetro no es un entero', () => {
+    expect(() => Fraction.mcd(1.5, 5)).toThrowError(Error);
+});
+
+test('Testing mcd: el segundo parámetro no es un entero', () => {
+    expect(() => Fraction.mcd(1.5, 5)).toThrowError(Error);
+});
+
+/*
+# Testing de mcm
+*/
+
+test('Testing mcm: es uno de los parámetros', () => {
     expect(Fraction.mcm(4, 2)).toBe(4);
-    // el mcm es la multiplicación de los parámetros
+});
+
+test('Testing mcm: es la multiplicación de los parámetros', () => {
     expect(Fraction.mcm(3, 2)).toBe(6);
-    // el mcm está entre el máximo de los parámetros 
-    //y la multiplicación de ambos(no incluidos)
+});
+
+test('Testing mcm: está entre el máximo de los parámetros y la multiplicación de ambos(no incluidos)', () => {
     expect(Fraction.mcm(4, 6)).toBe(12);
-    //el primer parámetro no es un entero
-    expect(() => Fraction.mcm(1.5, 5)).toThrowError(Error);
-    //el segunod parámetro no es un entero
+});
+
+test('Testing mcm: el primer parámetro no es un entero', () => {
+            expect(() => Fraction.mcm(1.5, 5)).toThrowError(Error);
+});
+
+test('Testing mcm: el segunod parámetro no es un entero', () => {
+            expect(() => Fraction.mcm(5, 1.5)).toThrowError(Error);
+});
+
+test('Testing mcm: el segunod parámetro no es un entero', () => {
     expect(() => Fraction.mcm(5, 1.5)).toThrowError(Error);
+});
+
+/*
+# Testing suma de fracciones
+*/
+
+test('Testing add: el parametro es un entero', () => {
+    let fr1 = new Fraction(2,4);
+    let fr2 = fr1.add(2);
+    expect(fr2.getNumerator()).toBe(5);
+    expect(fr2.getDenominator()).toBe(2);
+});
+
+test('Testing add: el parametro es otra fracción', () => {
+    let fr1 = new Fraction(2, 4);
+    let fr2 = new Fraction(1, 3);
+    let fr3 = fr1.add(fr2);
+    expect(fr3.getNumerator()).toBe(5);
+    expect(fr3.getDenominator()).toBe(6);
+});
+
+test('Testing add: el parametro no es una fracción ni un entero', () => {
+    let fr1 = new Fraction(2, 4);
+    let fr2 = fr1.add('a');
+    //hacer correctamente el TypeError
+    expect(fr2).toThrow(TypeError('Add method requires another fraction'));
+});
+
+/*
+# Testing multiplicacion de fracciones
+*/
+
+test('Testing multiply: el parametro es un entero', () => {
+    let fr1 = new Fraction(2, 4);
+    let fr2 = fr1.multiply(3);
+    expect(fr2.getNumerator()).toBe(3);
+    expect(fr2.getDenominator()).toBe(2);
+});
+
+test('Testing multiply: el parametro es otra fracción', () => {
+    let fr1 = new Fraction(2, 4);
+    let fr2 = new Fraction(3, 2);
+    let fr3 = fr1.multiply(fr2);
+    expect(fr3.getNumerator()).toBe(3);
+    expect(fr3.getDenominator()).toBe(4);
+});
+
+test('Testing multiply: el parametro no es una fracción ni un entero', () => {
+    let fr1 = new Fraction(2, 4);
+    let fr2 = fr1.multiply('a');
+    //hacer correctamente el TypeError
+    expect(fr2).toThrow(TypeError('Add method requires another fraction'));
+});
+
+/*
+# Testing divide de fracciones
+*/
+
+test('Testing divide: el parametro es un entero', () => {
+    let fr1 = new Fraction(2, 4);
+    let fr2 = fr1.divide(2);
+    expect(fr2.getNumerator()).toBe(1);
+    expect(fr2.getDenominator()).toBe(4);
+});
+
+test('Testing divide: el parametro es otra fracción', () => {
+    let fr1 = new Fraction(2, 4);
+    let fr2 = new Fraction(1, 3);
+    let fr3 = fr1.divide(fr2);
+    expect(fr3.getNumerator()).toBe(3);
+    expect(fr3.getDenominator()).toBe(2);
+});
+
+test('Testing divide: el parametro no es una fracción ni un entero', () => {
+    let fr1 = new Fraction(2, 4);
+    let fr2 = fr1.divide('a');
+    //hacer correctamente el TypeError
+    expect(fr2).toThrowError(TypeError('Add method requires another fraction'));
+});
+
+/*
+# Testing resta de fracciones
+*/
+
+test('Testing resta: el parametro es un entero', () => {
+    let fr1 = new Fraction(7, 4);
+    let fr2 = fr1.minus(1);
+    expect(fr2.getNumerator()).toBe(3);
+    expect(fr2.getDenominator()).toBe(4);
+});
+
+test('Testing resta: el parametro es otra fracción', () => {
+    let fr1 = new Fraction(2, 4);
+    let fr2 = new Fraction(1, 3);
+    let fr3 = fr1.minus(fr2);
+    expect(fr3.getNumerator()).toBe(3);
+    expect(fr3.getDenominator()).toBe(2);
+});
+
+test('Testing resta: el parametro no es una fracción ni un entero', () => {
+    let fr1 = new Fraction(2, 4);
+    let fr2 = fr1.minus('a');
+    //hacer correctamente el TypeError
+    expect(fr2).toThrow(TypeError('Add method requires another fraction'));
 });
