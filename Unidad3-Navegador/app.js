5de3e8800c1fcb13377be058ec7970d0159a2cec
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-11-05 12:05:04 +0300

    unidad 3

---
+++ Unidad3-Navegador/app.js
@@ -0,0 +1,22 @@
+/* console.log(document);
+console.log(document.head);
+console.log(document.body); */
+/* console.log(document.title);
+console.log(document.head.parentNode);
+console.log(document.head.parentNode.nextSibling.nextSibling);
+console.log(document.body.childNodes[1]); */
+/* let children = document.body.childNodes; */
+/* let children = Array.from(document.body.childNodes);
+children.log(typeof(children)); */
+/* for(let child of children){
+    console.log(child);
+} */
+
+/* let text = document.body.childNodes[3].textContent;
+eval(text); */
+
+/* console.log(document.body.lastChild.previousSibling.previousSibling.id);
+console.log(document.body.lastChild.previousSibling.previousSibling.innerHTML); */
+
+let text = document.body.children[1].textContent;
+eval(text);
